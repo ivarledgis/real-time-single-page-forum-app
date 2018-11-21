31b0b2ed37ee91637f4caf9f500add3b44896b38
ivarledgis90@gmail.com <ivarledgis90@gmail.com>
2018-11-21 10:55:12 +0545

    creating Migration,Model,Controler,Factories

---
+++ app/realtimeApp/Data/Model/Question.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\realtimeApp\Data\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+}
---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\realtimeApp\Data\Model\Question::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
